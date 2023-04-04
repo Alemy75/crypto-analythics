e2bfc2e356bdec8fd83acbee5af01ce1a5e34bb7
Alemax75 <alemax75962@gmail.com>
2023-04-04 21:01:56 +0300

    Допилил табличку

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -5,21 +5,30 @@ const CoinTable = ({ coins }) => {
 	console.log(coins)
 	return (
 		<table className={s.iksweb}>
+			<thead>
+				<tr>
+					<th style={{textAlign: 'left'}}>Наименование</th>
+					<th>Валюта</th>
+					<th>Стоимость</th>
+					<th>Изменение цены за 24 ч.</th>
+					<th>В обороте за 24 ч.</th>
+				</tr>
+			</thead>
 			<tbody>
 				{
 					coins.map((coin, index) => (
-						<tr className={index % 2 === 0 && s.second}>
+						<tr className={index % 2 === 0 ? s.second : s.first}>
 							<td>
 								<div className={s["coin-title"]}>
-									<img src={coin.image} alt="" />
+									<img src={coin.image} alt={coin.name} />
 									<h3>{coin.name}</h3>
 									<p>{coin.symbol}</p>
 								</div>
 							</td>
-							<td>{coin.current_price}</td>
-							<td>{coin.market_cap_change_percentage_24h}</td>
-							<td>{coin.price_change_percentage_24h}</td>
-							<td>{coin.total_volume}</td>
+							<td>{coin.current_price} $</td>
+							<td className={coin.market_cap_change_percentage_24h > 0 ? s.up : s.low}>{coin.market_cap_change_percentage_24h}</td>
+							<td className={coin.price_change_percentage_24h > 0 ? s.up : s.low} >{coin.price_change_percentage_24h}</td>
+							<td>{coin.total_volume} $</td>
 						</tr>
 					))
 				}
